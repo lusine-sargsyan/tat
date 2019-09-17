579f0c83e0c2c829049f6358d4566033061439ab
lusine <lusine.sargsyan71@outlook.com>
2019-09-17 15:54:03 +0400

    tat

---
+++ square/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace square {
+    class Program {
+
+        static void Main (string[] args) 
+        {
+            int a, b, c, d;
+            a = Int32.Parse(Console.ReadLine ());
+            b = Int32.Parse(Console.ReadLine ());
+            c = Int32.Parse(Console.ReadLine ());
+            d = b * b - 4 * a * c;
+            if (d >= 0)
+            {
+                Console.WriteLine ((-b + Math.Sqrt (d)) / 2 / a);
+
+                Console.WriteLine ((-b - Math.Sqrt (d)) / 2 / a);
+            } 
+            else 
+            {
+                Console.WriteLine ("error");
+            }
+        }
+    }
+}
